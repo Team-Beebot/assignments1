3fe1f2aa831f69d40feff883b98cd00440d8a72a
Ramesh Khadka <rkhadka296@gmail.com>
2021-12-10 16:46:39 +0545

    Update Answer3.java

--- src/Assignment1/Answer3.java
+++ src/Assignment1/Answer3.java
@@ -3,20 +3,37 @@ package Assignment1;
 import java.util.Scanner;
 
 public class Answer3 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[] listOfName = new String[5];
-       int  lengthOfArr = listOfName.length;
+public static void main(String[] args) {
+int n;
+String temp;
+Scanner s = new Scanner(System.in);
+System.out.print("Enter number of names you want to enter:");
+n= s.nextInt();
+String names[]= new String[n];
+Scanner s1 = new Scanner(System.in);
+System.out.println("Enter all the names");
+for(int i =0; i< n; i++)
+{
+names[i] = s1.nextLine();
+}
+for(int i =0; i< n; i++)
+{
+for (int j = i +1; j<n; j++)
+{
+if(names[i].compareTo(names[j])>0)
+{
+temp = names[i];
+names[i]= names[j];
+names[j]= temp;
+}
+}
+}
+System.out.print("Names in Sorted Order:");
+for (int i = 0;i<n-1; i++)
+{
+System.out.print(names[i]+",");
+}
+System.out.print(names[n-1]);
 
-       String[] stringsAlpha = new String[5]; // this is another array after sorting name in ascending order.
-
-        for(int i=0; i<lengthOfArr; i++){
-            System.out.println("Enter a name you want: ");
-            listOfName[i] = sc.next();
-
-
-        }
-
-
-    }
+}
 }
